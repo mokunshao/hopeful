192d4218729c37aaccd403d68a505b18a6c967f9
mokunshao <mokunshao@foxmail.com>
2019-10-23 03:29:18 +0800

    配置 webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+  mode: 'production',
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
