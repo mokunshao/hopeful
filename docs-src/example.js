290a10857a2a347400c82a70c95e3aa7dbe001e5
mokunshao <mokunshao@foxmail.com>
2019-11-10 00:02:48 +0800

    完善 tabs 组件

--- docs-src/example.js
+++ docs-src/example.js
@@ -1,6 +1,12 @@
 import './example.scss';
 import { Tabs } from '../src/index';
 
+function callback(value) {
+  console.log(value);
+}
+
 const tabs = new Tabs('tabs1');
+const tabs2 = new Tabs('tabs2', callback);
 
 tabs.init();
+tabs2.init();
--- src/js/Tabs.js
+++ src/js/Tabs.js
@@ -1,25 +1,41 @@
 export class Tabs {
-  constructor(id) {
+  constructor(id, callback) {
     this.main = document.getElementById(id);
     [this.head] = this.main.getElementsByClassName('tabs-head');
     this.tabs = this.main.getElementsByClassName('tab');
     this.panes = this.main.getElementsByClassName('pane');
     this.index = 0;
-    this.tabs[this.index].classList.add('active');
-    this.panes[this.index].classList.add('active');
+    this.getInitTabIndex();
+    this.changeTab(this.index);
+    this.callback = callback;
+  }
+
+  getInitTabIndex() {
+    Array.prototype.forEach.call(this.tabs, (item) => {
+      if (item.classList.contains('active')) {
+        this.index = Array.prototype.indexOf.call(this.tabs, item);
+      }
+    });
   }
 
   bindEvent() {
     this.head.addEventListener('click', (e) => {
+      if (this.callback) this.callback(e.target);
       const index = Array.prototype.indexOf.call(this.tabs, e.target);
       if (index !== -1) {
-        this.tabs[this.index].classList.remove('active');
-        this.panes[this.index].classList.remove('active');
-        this.index = index;
-        this.tabs[this.index].classList.add('active');
-        this.panes[this.index].classList.add('active');
+        this.changeTab(index);
       }
     });
+  }
+
+  changeTab(index) {
+    if (this.index !== index) {
+      this.tabs[this.index].classList.remove('active');
+      this.panes[this.index].classList.remove('active');
+      this.index = index;
+    }
+    this.tabs[this.index].classList.add('active');
+    this.panes[this.index].classList.add('active');
   }
 
   init() {
