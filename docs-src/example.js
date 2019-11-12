0ee8de884db186128017a7825cf2d32cf743b3b7
mokunshao <mokunshao@foxmail.com>
2019-11-12 16:29:50 +0800

    完善 autotabs

--- docs-src/example.js
+++ docs-src/example.js
@@ -2,7 +2,7 @@ import './example.scss';
 import { Tabs, AutoTabs } from '../src/index';
 
 const tabs = new Tabs('#tabs1', 2);
-const tabs2 = new AutoTabs('#tabs2', 2);
+const tabs2 = new AutoTabs('#tabs2');
 
 tabs.init();
 tabs2.init();
--- src/js/AutoTabs.js
+++ src/js/AutoTabs.js
@@ -1,10 +1,25 @@
 import { Tabs } from './Tabs';
 
-export function AutoTabs(id, index) {
+export function AutoTabs(id, index, duration) {
   Tabs.call(this, id, index);
+  this.lenght = this.tabs.lenght;
+  this.duration = duration || 3000;
+  this.start();
 }
 
 AutoTabs.prototype = new Tabs();
 AutoTabs.prototype.constructor = AutoTabs;
+
+AutoTabs.prototype.start = function () {
+  const max = this.tabs.length - 1;
+  let i = this.current;
+  setInterval(() => {
+    i += 1;
+    if (i > max) {
+      i = 0;
+    }
+    this.active(i);
+  }, this.duration);
+};
 
 export default AutoTabs;
